9ddd1da4219d925b818f7d2bf0f5b33ecf726861
VL <contact@varlogix.com>
2021-07-13 17:27:17 +0530

    Another change

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
   console.log('[hello-world] root handler called')
   res
     .set('x-powered-by', 'cyclic.sh')
-    .send('<h1>Hello World, again (from local)!</h1>')
+    .send('<h1>Hello World, again (from local, Again)!</h1>')
     .end()
 })
 
